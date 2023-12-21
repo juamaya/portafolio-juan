1eaf2e74f7ea05b823380bb1da132bf7b196122a
juamaya <marinamaya@hotmail.es>
2023-12-21 02:48:48 +0100

    Add enlaces

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ ScrollReveal().reveal(
   ".home-img, .services-container, .portafolio-box, .contact form",
   { origin: "bottom" }
 );
-ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content h1, .about-img ", { origin: "left" });
 ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 /*  =========================  typed js ============================*/
